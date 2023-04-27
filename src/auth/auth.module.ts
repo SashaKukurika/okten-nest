75458888398a919cc0db54e5b6b60d8a919133ee
SashaKukurika <Kukurika.sasha@gmail.com>
2023-04-27 19:31:21 +0300

    lesson4 login

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
+import { PrismaService } from '../core/orm/prisma.service';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { LocalStrategy } from './local.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'jwtConstants.secret',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    UsersService,
+    PrismaService,
+    JwtStrategy,
+  ],
+  exports: [AuthService],
+})
+export class AuthModule {}
